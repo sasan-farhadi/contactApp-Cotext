11710b15df4e7ace176b6ca45bb39cc2a15915df
Sasan <sasanfarhadi@gmail.com>
2024-09-20 23:17:52 +0330

    commit'

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,11 @@
 import styles from './Home.module.css'
 import { FcContacts } from "react-icons/fc";
-import inputs from './constant/inputs';
-import { useState } from 'react';
+import inputs from '../constant/inputs';
+import { createContext, useState } from 'react';
+
+
+
+export const contactContext = createContext()
 
 const Home = () => {
 
@@ -16,24 +20,28 @@ const Home = () => {
     }
 
     return (
-        <div className={styles.contact}>
-            <div className={styles.logo}>
-                <h1><FcContacts size={80} /></h1>
-            </div>
-            {
-                inputs.map((input, index) => {
-                    return (
-                        <input key={index}
-                            type={input.type}
-                            name={input.name}
-                            placeholder={input.placeholder}
-                            onChange={changeHandler}
-                            value={contact[input.name]} />
-                    )
-                })
-            }
-            <button>Add</button>
-        </div>
+        <>
+            <contactContext.Provider>
+                <div className={styles.contact}>
+                    <div className={styles.logo}>
+                        <h1><FcContacts size={80} /></h1>
+                    </div>
+                    {
+                        inputs.map((input, index) => {
+                            return (
+                                <input key={index}
+                                    type={input.type}
+                                    name={input.name}
+                                    placeholder={input.placeholder}
+                                    onChange={changeHandler}
+                                    value={contact[input.name]} />
+                            )
+                        })
+                    }
+                    <button>Add</button>
+                </div>
+            </contactContext.Provider>
+        </>
     )
 }
 
