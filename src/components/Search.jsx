4ec23fba1cb28f89ad7efa9c1ad9b2ffba65b65a
Sasan <sasanfarhadi@gmail.com>
2024-09-22 11:14:11 +0330

    search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,31 @@
+import { useContext, useState } from 'react'
+import styles from './Search.module.css'
+import { ContactContext } from '../App'
+
 const Search = () => {
+
+    const { contacts, setContacts } = useContext(ContactContext)
+    const [searchValue, setSearchValue] = useState([])
+
+    const searchHandler = (e) => {
+        const searchText = e.target.value
+        setSearchValue(contacts.filter(x => x.email === searchText.toLowerCase().trim()))
+        console.log(searchValue)
+    }
     return (
-        <div>
-            <input type="text" />
+        <div className={styles.search}>
+            <input
+                type="text"
+                placeholder='Search By Email ...'
+                name='search'
+                onChange={e => searchHandler(e)}
+            />
+            <div className={styles.searchlist}>
+                {
+                    searchValue.map(x => (<ul key={x.id}><li>{x.email}</li></ul>))
+                }
+
+            </div>
         </div>
     )
 }
